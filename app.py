69d220ed2476c8e6d59acd02326db9b65d3c901c
tonacastelan <tonatiuh.flores@konfio.mx>
2020-02-24 13:20:05 -0600

    fix not importing db when runnign

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from db import db
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
@@ -14,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'tona'
 api = Api(app)
 
-@app.before_first_request
-def create_tabled():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -27,5 +22,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
